e273b26353bed8294b0d1a64199b5cecd81e47ed
SergeyKorolenko <pavilion1900@gmail.com>
2025-03-29 20:48:26 +0300

    feature(): 69. Sqrt(x)

--- src/main/java/org/example/binarysearch/example/SearchInsertPosition.java
+++ src/main/java/org/example/binarysearch/example/SearchInsertPosition.java
@@ -22,7 +22,7 @@ public class SearchInsertPosition {
         int left = 0;
         int right = nums.length - 1;
         while (left <= right) {
-            int mid = left + (right - left) / 2;
+            int mid = (left + right) >>> 1;
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] > target) {
---
+++ src/main/java/org/example/binarysearch/task/Sqrt.java
@@ -0,0 +1,38 @@
+package org.example.binarysearch.task;
+
+public class Sqrt {
+
+    public static void main(String[] args) {
+//        69. Sqrt(x)
+//        Easy
+//
+//        Given a non-negative integer x, return the square root of x rounded down to the nearest integer.
+//        The returned integer should be non-negative as well.
+//        You must not use any built-in exponent function or operator.
+
+        System.out.println(mySqrt(0));  // 0
+        System.out.println(mySqrt(2));  // 1
+        System.out.println(mySqrt(4));  // 2
+        System.out.println(mySqrt(8));  // 2
+    }
+
+    public static int mySqrt(int x) {
+        if (x < 2) {
+            return x;
+        }
+        int left = 2;
+        int right = x / 2;
+        while (left <= right) {
+            int mid = (left + right) >>> 1;
+            long sqMid = (long) mid * mid;
+            if (sqMid == x) {
+                return mid;
+            } else if (sqMid > x) {
+                right = --mid;
+            } else {
+                left = ++mid;
+            }
+        }
+        return right;
+    }
+}
