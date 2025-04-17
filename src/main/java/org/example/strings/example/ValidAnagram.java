d8ddbf7eb159bcbdd08c818c13084ef0c376ddb8
SergeyKorolenko <pavilion1900@gmail.com>
2025-04-17 21:31:45 +0300

    feature(): 242. Valid Anagram

---
+++ src/main/java/org/example/strings/example/ValidAnagram.java
@@ -0,0 +1,31 @@
+package org.example.strings.example;
+
+public class ValidAnagram {
+
+//        242. Valid Anagram
+//        Easy
+//
+//        Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram"));  // true
+        System.out.println(isAnagram("rat", "car"));          // false
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] counter = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
+        }
+        for (int num : counter) {
+            if (num != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/main/java/org/example/strings/task/ValidAnagram.java
@@ -0,0 +1,18 @@
+package org.example.strings.task;
+
+public class ValidAnagram {
+
+//        242. Valid Anagram
+//        Easy
+//
+//        Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram"));  // true
+        System.out.println(isAnagram("rat", "car"));          // false
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        return false;
+    }
+}
