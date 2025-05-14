c7c1905b302f56c70021f9cb1f09f7627877a491
SergeyKorolenko <pavilion1900@gmail.com>
2025-05-14 10:40:05 +0300

    feature(): 21. Merge Two Sorted Lists

---
+++ src/main/java/org/example/linkedlists/example/MergeTwoSortedLists.java
@@ -0,0 +1,77 @@
+package org.example.linkedlists.example;
+
+public class MergeTwoSortedLists {
+
+//    21. Merge Two Sorted Lists
+//    Easy
+//
+//    You are given the heads of two sorted linked lists list1 and list2.
+//
+//    Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+//
+//    Return the head of the merged linked list.
+
+    public static void main(String[] args) {
+        ListNode6 node1 = new ListNode6(1);
+        ListNode6 node2 = new ListNode6(2);
+        ListNode6 node3 = new ListNode6(4);
+        node1.setNext(node2);
+        node2.setNext(node3);
+
+        ListNode6 node11 = new ListNode6(1);
+        ListNode6 node21 = new ListNode6(3);
+        ListNode6 node31 = new ListNode6(4);
+        node11.setNext(node21);
+        node21.setNext(node31);
+
+        ListNode6 mergedHead = mergeTwoLists(node1, node11);
+        printNodesValue(mergedHead); // 1 1 2 3 4 4
+    }
+
+    public static ListNode6 mergeTwoLists(ListNode6 list1, ListNode6 list2) {
+        ListNode6 preHead = new ListNode6(-1);
+        ListNode6 prev = preHead;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                prev.next = list1;
+                list1 = list1.next;
+            } else {
+                prev.next = list2;
+                list2 = list2.next;
+            }
+            prev = prev.next;
+        }
+        prev.next = list1 == null ? list2 : list1;
+        return preHead.next;
+    }
+
+    private static void printNodesValue(ListNode6 head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+}
+
+class ListNode6 {
+
+    int val;
+    ListNode6 next;
+
+    ListNode6() {
+    }
+
+    ListNode6(int val) {
+        this.val = val;
+    }
+
+    ListNode6(int val, ListNode6 next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public void setNext(ListNode6 next) {
+        this.next = next;
+    }
+}
