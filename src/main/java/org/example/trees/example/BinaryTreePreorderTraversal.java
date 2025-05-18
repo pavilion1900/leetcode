15fddff3963073c66165c4c62a37f51beb1ddfa1
SergeyKorolenko <pavilion1900@gmail.com>
2025-05-18 22:03:01 +0300

    feature(): 144. Binary Tree Preorder Traversal

---
+++ src/main/java/org/example/trees/example/BinaryTreePreorderTraversal.java
@@ -0,0 +1,85 @@
+package org.example.trees.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class BinaryTreePreorderTraversal {
+
+//    144. Binary Tree Preorder Traversal
+//    Easy
+//
+//    Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+    public static void main(String[] args) {
+        TreeNode1 node1 = new TreeNode1(3);
+        TreeNode1 node2 = new TreeNode1(2, node1, null);
+        TreeNode1 node3 = new TreeNode1(1, null, node2);
+        preorderTraversal(node3)
+                .forEach(num -> System.out.print(num + " ")); // 1 2 3
+        System.out.println();
+
+        TreeNode1 node4 = new TreeNode1(6);
+        TreeNode1 node5 = new TreeNode1(7);
+        TreeNode1 node6 = new TreeNode1(5, node4, node5);
+        TreeNode1 node7 = new TreeNode1(4);
+        TreeNode1 node8 = new TreeNode1(2, node7, node6);
+        TreeNode1 node9 = new TreeNode1(9);
+        TreeNode1 node10 = new TreeNode1(8, node9, null);
+        TreeNode1 node11 = new TreeNode1(3, null, node10);
+        TreeNode1 node12 = new TreeNode1(1, node8, node11);
+        preorderTraversal(node12)
+                .forEach(num -> System.out.print(num + " ")); // 1 2 4 5 6 7 3 8 9
+        System.out.println();
+
+        TreeNode1 node13 = null;
+        preorderTraversal(node13)
+                .forEach(num -> System.out.print(num + " ")); //
+        System.out.println();
+
+        TreeNode1 node14 = new TreeNode1(1);
+        preorderTraversal(node14)
+                .forEach(num -> System.out.print(num + " ")); // 1
+        System.out.println();
+    }
+
+    public static List<Integer> preorderTraversal(TreeNode1 root) {
+        Stack<TreeNode1> stack = new Stack<>();
+        List<Integer> rsl = new ArrayList<>();
+        if (root == null) {
+            return rsl;
+        }
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode1 node = stack.pop();
+            rsl.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return rsl;
+    }
+}
+
+class TreeNode1 {
+
+    int val;
+    TreeNode1 left;
+    TreeNode1 right;
+
+    TreeNode1() {
+    }
+
+    TreeNode1(int val) {
+        this.val = val;
+    }
+
+    TreeNode1(int val, TreeNode1 left, TreeNode1 right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
