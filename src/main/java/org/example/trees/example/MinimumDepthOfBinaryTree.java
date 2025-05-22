3d14d6d3e0bbaa9e572508e8baed2d88fcde92e7
SergeyKorolenko <pavilion1900@gmail.com>
2025-05-22 22:52:54 +0300

    feature(): 111. Minimum Depth of Binary Tree

---
+++ src/main/java/org/example/trees/example/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,61 @@
+package org.example.trees.example;
+
+public class MinimumDepthOfBinaryTree {
+
+//    111. Minimum Depth of Binary Tree
+//    Easy
+//
+//    Given a binary tree, find its minimum depth.
+//
+//    The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+//
+//    Note: A leaf is a node with no children.
+
+    public static void main(String[] args) {
+        TreeNode5 node1 = new TreeNode5(15);
+        TreeNode5 node2 = new TreeNode5(7);
+        TreeNode5 node3 = new TreeNode5(20, node1, node2);
+        TreeNode5 node4 = new TreeNode5(9);
+        TreeNode5 node5 = new TreeNode5(3, node4, node3);
+        System.out.println(minDepth(node5)); // 2
+
+        TreeNode5 node6 = new TreeNode5(6);
+        TreeNode5 node7 = new TreeNode5(5, null, node6);
+        TreeNode5 node8 = new TreeNode5(4, null, node7);
+        TreeNode5 node9 = new TreeNode5(3, null, node8);
+        TreeNode5 node10 = new TreeNode5(2, null, node9);
+        System.out.println(minDepth(node10)); // 5
+    }
+
+    public static int minDepth(TreeNode5 root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null) {
+            return 1 + minDepth(root.right);
+        } else if (root.right == null) {
+            return 1 + minDepth(root.left);
+        }
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+    }
+}
+
+class TreeNode5 {
+
+    int val;
+    TreeNode5 left;
+    TreeNode5 right;
+
+    TreeNode5() {
+    }
+
+    TreeNode5(int val) {
+        this.val = val;
+    }
+
+    TreeNode5(int val, TreeNode5 left, TreeNode5 right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
