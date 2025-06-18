3c03c7634b243d88d64d36bea768a6cef71e220d
SergeyKorolenko <pavilion1900@gmail.com>
2025-06-18 21:08:41 +0300

    feature(): 477. Total Hamming Distance

---
+++ src/main/java/org/example/bit/example/TotalHammingDistance.java
@@ -0,0 +1,31 @@
+package org.example.bit.example;
+
+public class TotalHammingDistance {
+
+//    477. Total Hamming Distance
+//    Medium
+//
+//    The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+//
+//    Given an integer array nums, return the sum of Hamming distances between all the pairs of the integers in nums.
+
+    public static void main(String[] args) {
+        System.out.println(totalHammingDistance(new int[]{4, 14, 2})); // 6
+        System.out.println(totalHammingDistance(new int[]{4, 14, 4})); // 4
+    }
+
+    public static int totalHammingDistance(int[] nums) {
+        int[] countBits = new int[31];
+        for (int num : nums) {
+            for (int i = 0; i < 31; i++) {
+                countBits[i] += num & 1;
+                num >>= 1;
+            }
+        }
+        int rsl = 0;
+        for (int i = 0; i < 31; i++) {
+            rsl += countBits[i] * (nums.length - countBits[i]);
+        }
+        return rsl;
+    }
+}
---
+++ src/main/java/org/example/bit/task/TotalHammingDistance.java
@@ -0,0 +1,20 @@
+package org.example.bit.task;
+
+public class TotalHammingDistance {
+
+//    477. Total Hamming Distance
+//    Medium
+//
+//    The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+//
+//    Given an integer array nums, return the sum of Hamming distances between all the pairs of the integers in nums.
+
+    public static void main(String[] args) {
+        System.out.println(totalHammingDistance(new int[]{4, 14, 2})); // 6
+        System.out.println(totalHammingDistance(new int[]{4, 14, 4})); // 4
+    }
+
+    public static int totalHammingDistance(int[] nums) {
+        return -1;
+    }
+}
