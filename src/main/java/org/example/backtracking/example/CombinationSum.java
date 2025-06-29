fbc99ae012bf6a488cb5f4429ae551b01fcbcc8c
SergeyKorolenko <pavilion1900@gmail.com>
2025-06-29 17:39:40 +0300

    feature(): 39. Combination Sum

---
+++ src/main/java/org/example/backtracking/example/CombinationSum.java
@@ -0,0 +1,45 @@
+package org.example.backtracking.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum {
+
+//    39. Combination Sum
+//    Medium
+//
+//    Given an array of distinct integers candidates and a target integer target, return a list of all unique
+//    combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+//
+//    The same number may be chosen from candidates an unlimited number of times. Two combinations are unique
+//    if the frequency of at least one of the chosen numbers is different.
+//
+//    The test cases are generated such that the number of unique combinations that sum up to target is less than
+//    150 combinations for the given input.
+
+    public static void main(String[] args) {
+        System.out.println(combinationSum(new int[]{2, 3, 6, 7}, 7)); // [[2, 2, 3], [7]]
+        System.out.println(combinationSum(new int[]{2, 3, 5}, 8));    // [[2, 2, 2, 2], [2, 3, 3], [3, 5]]
+        System.out.println(combinationSum(new int[]{2}, 1));          // []
+    }
+
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> rsl = new ArrayList<>();
+        backtrack(candidates, target, rsl, new ArrayList<>(), 0);
+        return rsl;
+    }
+
+    private static void backtrack(int[] candidates, int target, List<List<Integer>> rsl, List<Integer> comb, int start) {
+        if (target == 0) {
+            rsl.add(new ArrayList<>(comb));
+            return;
+        } else if (target < 0) {
+            return;
+        }
+        for (int i = start; i < candidates.length; i++) {
+            comb.add(candidates[i]);
+            backtrack(candidates, target - candidates[i], rsl, comb, i);
+            comb.remove(comb.size() - 1);
+        }
+    }
+}
