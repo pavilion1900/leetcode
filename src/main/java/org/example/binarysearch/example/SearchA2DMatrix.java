79765fb16d07abb60bb9f2b8dcd57fc8e777dc7b
SergeyKorolenko <pavilion1900@gmail.com>
2025-07-22 23:23:05 +0300

    feature(): refactoring

--- src/main/java/org/example/binarysearch/example/SearchA2DMatrix.java
+++ src/main/java/org/example/binarysearch/example/SearchA2DMatrix.java
@@ -11,12 +11,20 @@ public class SearchA2DMatrix {
 //    Given an integer target, return true if target is in matrix or false otherwise.
 
     public static void main(String[] args) {
-        System.out.println(searchMatrix(new int[][]{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}}, 3));  // true
-        System.out.println(searchMatrix(new int[][]{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}}, 13)); // false
+        System.out.println(searchMatrix(new int[][]{
+                {1, 3, 5, 7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 60}
+        }, 3));  // true
+        System.out.println(searchMatrix(new int[][]{
+                {1, 3, 5, 7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 60}
+        }, 13)); // false
     }
 
 //    Approach 1: Binary Search
-//    Time complexity  : O(log(nm))
+//    Time complexity  : O(log(n * m))
 //    Space complexity : O(1)
 
     public static boolean searchMatrix(int[][] matrix, int target) {
